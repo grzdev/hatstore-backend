6d06a6bbd53b675e3278bda179e1f95aada7e41f
zay <damilolaoyeniyi13@gmail.com>
2023-09-02 18:55:57 +0100

    artcile bug fix

--- models/Articles.js
+++ models/Articles.js
@@ -4,25 +4,20 @@ const ArticlesSchema = new mongoose.Schema({
     title: {type: String, required: true},
     bgCover: {type: String, required: true},
     desc: {type: String, required: true},
-    articleBody: [
-        {
-            subheading1: {type: String, required: true},
-            body1: {type: String, required: true},
+    subheading1: {type: String, required: true},
+    body1: {type: String, required: true},
 
-            subheading2: {type: String, required: true},
-            body2: {type: String, required: true},
+    subheading2: {type: String, required: true},
+    body2: {type: String, required: true},
 
-            subheading3: {type: String, required: true},
-            body3: {type: String, required: true},
+    subheading3: {type: String, required: true},
+    body3: {type: String, required: true},
 
-            subheading4: {type: String, required: false},
-            body4: {type: String, required: false},
+    subheading4: {type: String, required: false},
+    body4: {type: String, required: false},
 
-            subheading5: {type: String, required: false},
-            body5: {type: String, required: false},
-
-        }
-    ],
+    subheading5: {type: String, required: false},
+    body5: {type: String, required: false},
     dateMade: {type: String, required: true},
     minRead: {type: String, required: true}, 
     writtenBy: {type: String, required: true}, 
