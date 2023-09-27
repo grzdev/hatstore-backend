40b2e2a773d99ad049e69a9a94e10bee8865a6bb
zay <damilolaoyeniyi13@gmail.com>
2023-09-27 22:04:11 +0100

    Middleware bug

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,7 +4,7 @@ const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token;
 
     if(authHeader){
-        const token = authHeaders.split(" ")[1];
+        const token = authHeader.split(" ")[1];
 
         jwt.verify(token, process.env.JWT_PASSWORD, async (err, user) => {
             if(err){{
