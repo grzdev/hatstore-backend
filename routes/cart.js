372bdbbf8a9f5755078683722b648d8f527823c8
zay <damilolaoyeniyi13@gmail.com>
2023-09-28 14:08:03 +0100

    Test

--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,7 @@ const cartController = require("../controllers/cartController")
 const {verifyToken} = require("../middleware/verifyToken")
 
 router.get("/find/:id", cartController.getCart)
-router.post("/", cartController.addToCart)
+router.post("/", verifyToken, cartController.addToCart)
 router.delete("/:cartItemId", cartController.deleteCartItem)
 router.post("/quantity", cartController.decreaseCartItem)
 
