8ea14a3d23035b698d14bbfc7e777a6b13ada2be
zay <damilolaoyeniyi13@gmail.com>
2023-10-08 08:38:21 +0100

    Products 2 schema

--- models/Articles.js
+++ models/Articles.js
@@ -2,21 +2,9 @@ const mongoose = require("mongoose")
 
 const ArticlesSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    bgCover: {type: String, required: true},
-    desc: {type: String, required: true},
-    subheading1: {type: String, required: true},
-    body1: {type: String, required: true},
-    subheading2: {type: String, required: true},
-    body2: {type: String, required: true},
-    subheading3: {type: String, required: true},
-    body3: {type: String, required: true},
-    subheading4: {type: String, required: false},
-    body4: {type: String, required: false},
-    subheading5: {type: String, required: false},
-    body5: {type: String, required: false},
-    dateMade: {type: String, required: true},
-    minRead: {type: String, required: true}, 
-    writtenBy: {type: String, required: true}, 
+    imageUrl: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: String, required: true}, 
 }, {timestamps: true});
 
 module.exports = mongoose.model("Article", ArticlesSchema) 
