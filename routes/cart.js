f64b58ee458655f1038f743037e216c6d9e39da6
zay <damilolaoyeniyi13@gmail.com>
2023-10-16 11:06:42 +0100

    bug fix

--- routes/cart.js
+++ routes/cart.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const cartController = require("../controllers/cartController")
 const {verifyToken} = require("../middleware/verifyToken")
 
-router.get("/find:id", cartController.getCart)
+router.get("/find/:id", cartController.getCart)
 router.post("/", cartController.addToCart)
 router.delete("/:cartItemId", cartController.deleteCartItem)
 router.post("/quantity", cartController.decreaseCartItem)
